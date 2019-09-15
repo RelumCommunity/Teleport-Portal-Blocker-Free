9aacc83588c5eab8c1cde69699ba958d75b0c09b
VainceCraft <51292267+vaincecraft@users.noreply.github.com>
2019-09-15 19:39:42 +0200

    Update NPEvent.java

--- com.vaincecraft.portalblockerfree/events/NPEvent.java
+++ com.vaincecraft.portalblockerfree/events/NPEvent.java
@@ -4,6 +4,7 @@ import org.bukkit.Achievement;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.advancement.Advancement;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,53 +17,57 @@ import com.vaincecraft.portalblockerfree.main.Main;
 public class NPEvent implements Listener {
 	@EventHandler
 	public void PortalEnter (PlayerPortalEvent i) {
-		String prefixs = Main.getInstance().getConfig().getString("Prefix");
-		String prefix = prefixs.replaceAll("&", "§");
-		String netherportale = Main.getInstance().getConfig().getString("NetherPortal.QuestName");
-		String netherpe = netherportale.replaceAll("&", "§");
-		String netherportalerror = Main.getLangFile().getString("error.netherportal");
-		String npe = netherportalerror.replaceAll("&", "§");
-		String colormsg1 = npe.replaceAll("%netherportalquest%", netherpe);
-		String netherportalpermblockmessage = Main.getLangFile().getString("permblockteleport.netherportal");
-		String colormsg2 = netherportalpermblockmessage.replaceAll("&", "§");
-		if (!Main.getInstance().getConfig().getBoolean("PermBlockTeleports.NPBlock")) {
-			if (Main.getInstance().getConfig().getBoolean("Teleports.NPBlock")) {
-				if (i.getCause().equals(TeleportCause.NETHER_PORTAL)) {
-					if (Bukkit.getVersion().contains("1.12") || Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.14")){
-						if (hasAdvancement(i.getPlayer())) {
+		if (i.getCause().equals(TeleportCause.NETHER_PORTAL)) {
+			Player p = i.getPlayer();
+			String ver = Bukkit.getVersion();
+			FileConfiguration main = Main.getInstance().getConfig();
+			String prefixs = main.getString("Prefix");
+			String prefix = prefixs.replaceAll("&", "§");
+			String netherportale = main.getString("NetherPortal.QuestName");
+			String netherpe = netherportale.replaceAll("&", "§");
+			String netherportalerror = Main.getLangFile().getString("error.netherportal");
+			String npe = netherportalerror.replaceAll("&", "§");
+			String colormsg1 = npe.replaceAll("%netherportalquest%", netherpe);
+			String netherportalpermblockmessage = Main.getLangFile().getString("permblockteleport.netherportal");
+			String colormsg2 = netherportalpermblockmessage.replaceAll("&", "§");
+			if (!main.getBoolean("PermBlockTeleports.NPBlock")) {
+				if (main.getBoolean("BlockThroughAchievement.NPBlock")) {
+					if (ver.contains("1.12") || ver.contains("1.13") || ver.contains("1.14")) {
+						if (hasAdvancement(p)) {
 							return;
 						}
 						else {
 							i.setCancelled(true);
-							if (Main.getInstance().getConfig().getBoolean("NetherPortal.ErrorMessage")) {
-								i.getPlayer().sendMessage(prefix + colormsg1);
+							if (main.getBoolean("NetherPortal.ErrorMessage")) {
+								p.sendMessage(prefix + colormsg1);
 							}
 						}
 					}
 					else {
-						String ach = Main.getInstance().getConfig().getString("NetherPortal.Achievement");
-						if (i.getPlayer().hasAchievement(Achievement.valueOf(ach))) {
+						String ach = main.getString("NetherPortal.Achievement");
+						if (p.hasAchievement(Achievement.valueOf(ach))) {
 							return;
 						}
 						else {
 							i.setCancelled(true);
-							if (Main.getInstance().getConfig().getBoolean("NetherPortal.ErrorMessage")) {
-								i.getPlayer().sendMessage(prefix + colormsg1);
+							if (main.getBoolean("NetherPortal.ErrorMessage")) {
+								p.sendMessage(prefix + colormsg1);
 							}
 						}
 					}
 				}
 			}
-		}
-		else {
-			i.setCancelled(true);
-			if (Main.getInstance().getConfig().getBoolean("NetherPortal.PermBlockErrorMessage")) {
-				i.getPlayer().sendMessage(prefix + colormsg2);
+			else {
+				i.setCancelled(true);
+				if (main.getBoolean("NetherPortal.PermBlockErrorMessage")) {
+					p.sendMessage(prefix + colormsg2);
+				}
 			}
 		}
 	}
 	public static boolean hasAdvancement(Player p) {
-		String adv = Main.getInstance().getConfig().getString("NetherPortal.Advancement");
+		FileConfiguration main = Main.getInstance().getConfig();
+		String adv = main.getString("NetherPortal.Advancement");
 		Advancement advancement = Bukkit.getAdvancement(NamespacedKey.minecraft(adv));
 		if (p.getAdvancementProgress(advancement).isDone()) {
 			return true;
